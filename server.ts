8fea05fcb63c9e17a905da616dd206fd02cb7cd9
Icaro Vieira <soprafazermissal@gmail.com>
2022-02-14 20:43:01 -0300

    correção de erros

--- server.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { resolve } from "path";
-const app = express();
-
-app.use("/", express.static(resolve(__dirname, "./buid")));
-
-app.listen(process.env.PORT || 3000);
